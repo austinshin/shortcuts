5c9f2291cd57788768d867448eec449d0ce4dc79
Austin Shin <shinaustin@gmail.com>
2018-02-01 19:22:15 -0800

    shortcuts commit

---
+++ shortcuts.js
@@ -0,0 +1,125 @@
+/*
+ * @author: Austin Shin
+ * a shortcuts presentation 
+ */
+
+class shortcuts {
+  constructor() {}
+
+  // alt + shift + up/down to move around
+  movingUpAndDownOneRow() {
+    let x = 0;
+    let x = 1;
+    let x = 2;
+  }
+
+  deleteRow() {
+  	return 'cmd + backspace';
+    return 'cmd + shift + left/right;
+    return 'cmd + shift + left/right || cmd + backspace';
+  }
+
+  fixMe() {
+  	const x = 5;
+  	x = 4;
+  	return 'alt + shift + left/right';
+  }
+
+  fixIndents() {
+let hrsf87chant = "hr87!";
+hrsf87chant = "Crank it to Eleven";
+hrsf87chant = "There's nobody named Kevin";
+      hrsf87chant = "Shoutout to McLovin";
+		hrsf87chant = ":O"; 	
+		return 'fn + tab' || 'fn + shift tab || cmd + [ or ]';
+  }
+
+  commentThisOut() {
+    Hello
+    How
+    Are
+    You
+    return 'cmd + / || cmd + alt + /'
+  }  
+
+// ctrl + g || cmd + t || cmd + p
+
+
+
+
+
+
+
+
+
+
+
+
+  goToLineNumber() {
+  	//make this true
+    let x = 'FILL ME IN';
+    return x === 'Hello!';
+  }
+ 
+  goToTopBot() {
+    return 'cmd + down' || 'cmd + up';
+  }
+
+  goToBeginningOrEnd() {
+    return 'cmd + left' || 'cmd + right';
+  }
+
+
+  removeTodosMultipleTextHighlight() {
+  	//TODO
+  	//TODO
+  	//TODO
+  	return 'cmd + d || cmd + u';
+  }
+
+  consoleLogMultipleThings() {
+
+
+
+    return 'cmd + shift + L'
+  }
+  
+
+  readjustLines() {
+  	y = x + 3;
+    let y = 1;
+    let x = 1 + 1;
+    return y || 'cmd + ctrl + up/down';
+  }
+
+  closeThisFileThenReopn() {
+  	return 'cmd + s -> cmd + w -> cmd + shift + t';
+  }
+
+  splitPane() {
+    return 'cmd + alt + 2';
+  }
+
+/*
+google chrome hotkeys
+highlight search bar - cmd + L
+bookmarking localhost:3000 or whatever for quick access
+move tab right - ctrl + tab 
+move tab left - ctrl + shift + tab
+cmd + 1 = go to first tab
+cmd + 9 = go to last tab
+cmd + 2-8 = numbers (tab organization)
+
+better touch tool -> for more hotkeys i.e window snapping that mac usually doesnt provide. 
+
+cmd line 
+pause whatever you are doing -> ctrl z 
+bring back -> fg
+cmd d for split console vertically
+cmd shift d for split console horizontally 
+
+make a file and use nodemon on it
+*/
+
+}
+
