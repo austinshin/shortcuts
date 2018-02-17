1c7e19cf646ea003a37a70e37f6f371cf70c2ff8
Austin Shin <shinaustin@gmail.com>
2018-02-16 17:46:11 -0800

    updated some stuff

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # shortcuts
+
+terminal recommendation: iterm2 (custom hotkeys, tabs, terminal multiplexer, custom colors, built in
+tmux)
+terminal plugin: tmux/zshrc - sessions/multiplexer/plugins
+
+mac useful tools:
+= signs means they do the equivalent with slightly different features. the first one in each list is
+what I use.
+1. insomniaX = lighthead = amphetamine - disable idle/lid sleep config
+2. insomnia = postman = curl - send http requests
+3. siege npm = siege brew install = artillery - npm modules to stress test
+4. bettertouchtools = spectale - hotkeys for your mac and window snapping
+5. trello - ticketing website (there's an extension on chrome to create parent/child relationships
+   easily)
+6. docker - setting up containers/vm for your app
+
+text editors: vim/vscode/atom/sublime text
+
+must have plugins for your text editor-
+learn shortcuts - most are universal
+snippets - example here https://github.com/SirVer/ultisnips - applies to not only vim
+prettier - example here https://github.com/prettier/vim-prettier - applies to not only vim
+eslint - airbnb or google config
+
+
+
+
--- shortcuts.js
+++ shortcuts.js
@@ -1,6 +1,8 @@
 /*
  * @author: Austin Shin
- * a shortcuts presentation 
+ * A shortcuts presentation
+ * This was done in sublime text 3
+ * Many of these are applicable to vscode/atom.
  */
 
 class shortcuts {
@@ -30,7 +32,7 @@ let hrsf87chant = "hr87!";
 hrsf87chant = "Crank it to Eleven";
 hrsf87chant = "There's nobody named Kevin";
       hrsf87chant = "Shoutout to McLovin";
-		hrsf87chant = ":O"; 	
+		hrsf87chant = ":O";
 		return 'fn + tab' || 'fn + shift tab || cmd + [ or ]';
   }
 
@@ -40,7 +42,7 @@ hrsf87chant = "There's nobody named Kevin";
     Are
     You
     return 'cmd + / || cmd + alt + /'
-  }  
+  }
 
 // ctrl + g || cmd + t || cmd + p
 
@@ -60,7 +62,7 @@ hrsf87chant = "There's nobody named Kevin";
     let x = 'FILL ME IN';
     return x === 'Hello!';
   }
- 
+
   goToTopBot() {
     return 'cmd + down' || 'cmd + up';
   }
@@ -83,7 +85,7 @@ hrsf87chant = "There's nobody named Kevin";
 
     return 'cmd + shift + L'
   }
-  
+
 
   readjustLines() {
   	y = x + 3;
@@ -101,24 +103,31 @@ hrsf87chant = "There's nobody named Kevin";
   }
 
 /*
-google chrome hotkeys
+google chrome hotkeys - many of these applies to terminal too
 highlight search bar - cmd + L
 bookmarking localhost:3000 or whatever for quick access
-move tab right - ctrl + tab 
+move tab right - ctrl + tab
 move tab left - ctrl + shift + tab
 cmd + 1 = go to first tab
 cmd + 9 = go to last tab
 cmd + 2-8 = numbers (tab organization)
 
-better touch tool -> for more hotkeys i.e window snapping that mac usually doesnt provide. 
-
-cmd line 
-pause whatever you are doing -> ctrl z 
+cmd line
+pause whatever you are doing -> ctrl z
 bring back -> fg
-cmd d for split console vertically
-cmd shift d for split console horizontally 
+cmd d for split console vertically (iterm)
+cmd shift d for split console horizontally (iterm)
+use screen or tmux in default terminal
 
-make a file and use nodemon on it
+make a file and use nodemon on it for consistent testing.
+
+if you want to test something quickly and want to install npm packages
+mkdir testdir
+npm init -y
+npm install 'whatever you need'
+touch test.js //do whatever you want to test
+node(mon) test.js
+you can also install mocha/chai to test too
 */
 
 }
